00d231fd9c13cd3d7a4eeb1960a653afb19d6eb4
zygals <2799024884@qq.com>
2017-08-29 17:26:12 +0800

    OrderGood.php gai

--- app/index/model/OrderGood.php
+++ app/index/model/OrderGood.php
@@ -15,7 +15,7 @@ class OrderGood extends Model
             $where = ['order_id'=>$order_id];
 
         }
-        $list=$this->alias('og')->order('create_time desc')->where($where)->field('og.*,g.name good_name,g.price_now,g.price_original,g.img,g.img_thumb,order.trade_no')->join('order','order.id=og.order_id')->join('good g','g.id=og.good_id')->paginate(config('paginate.list_rows'));
+        $list=$this->alias('og')->order('g.create_time desc')->where($where)->field('og.*,g.name good_name,g.price_now,g.price_original,g.img,g.img_thumb,order.trade_no')->join('order','order.id=og.order_id')->join('good g','g.id=og.good_id')->paginate(config('paginate.list_rows'));
 
 		return $list;
 	}
